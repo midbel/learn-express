4ad38ea7ec8f22848b5e276b81f90ba3e0f5df7f
midbel <brun.nicolas21@gmail.com>
2022-03-19 09:49:02 +0100

    reorganize code

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,7 @@
 const Joi = require('joi')
+const jwt = require('./token.js')
+const BasicStrategy = require('passport-http').BasicStrategy
+const BearerStrategy = require('passport-http-bearer').Strategy
 
 exports.validate = function(schema) {
   return (req, res, next) => {
@@ -10,3 +13,25 @@ exports.validate = function(schema) {
     }
   }
 }
+
+const testuser = {
+  user: 'admin',
+  pass: 'supersecretpassword'
+}
+
+exports.basic = new BasicStrategy((user, pass, done) => {
+  if (user === testuser.user && pass === testuser.pass) {
+    done(null, testuser)
+    return
+  }
+  done(new Error("invalid credentials provided"))
+})
+
+exports.bearer = new BearerStrategy((token, done) => {
+  try {
+    const data = jwt.verify(token, secretkey)
+    done(null, data)
+  } catch(err) {
+    done("invalid token provided")
+  }
+})
